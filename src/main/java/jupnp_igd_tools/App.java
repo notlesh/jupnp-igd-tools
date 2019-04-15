ea6a3fc9985d0ef528d8b50c07ec131f9cb617fe
Stephen Shelton <stephen@stephenashelton.com>
2019-04-15 11:27:03 -0600

    Stub out command line parsing

--- src/main/java/jupnp_igd_tools/App.java
+++ src/main/java/jupnp_igd_tools/App.java
@@ -3,12 +3,37 @@
  */
 package jupnp_igd_tools;
 
+import jupnp_igd_tools.CLI;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.ParseException;
+
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
+
+    public static final String APP_NAME = "igd-tool";
+
+    public static final int RETURN_PARSE_ERROR = 1;
+    public static final int RETURN_UNMATCHED_CLI_ARGS = 2;
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        CLI cli = new CLI();
+        CommandLine cl = null;
+
+        try {
+            cl = cli.parse(args);
+        } catch (ParseException e) {
+            System.err.println("Error parsing command line args: "+ e);
+            System.exit(RETURN_PARSE_ERROR);
+        }
+
+        if (cl.hasOption("e")) {
+            System.out.println("TODO: Implement");
+        } else if (cl.hasOption("h")) {
+            cli.printUsageText(APP_NAME);
+        } else {
+            cli.printUsageText(APP_NAME);
+            System.exit(RETURN_UNMATCHED_CLI_ARGS);
+        }
+
     }
 }
