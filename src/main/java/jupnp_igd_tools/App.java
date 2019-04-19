aa75e753a98b059e75fd3ac0c608aaabd81d7359
Stephen Shelton <stephen@stephenashelton.com>
2019-04-19 12:29:02 -0600

    Implement querying for ExternalIPAddress

--- src/main/java/jupnp_igd_tools/App.java
+++ src/main/java/jupnp_igd_tools/App.java
@@ -4,18 +4,34 @@
 package jupnp_igd_tools;
 
 import jupnp_igd_tools.CLI;
+import jupnp_igd_tools.igd.NatUpnpManager;
+import jupnp_igd_tools.igd.ExternalIpQuery;
+
+import org.jupnp.model.meta.Service;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.ParseException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CompletableFuture;
+
 public class App {
+
+    private static final Logger log = LoggerFactory.getLogger(App.class);
 
     public static final String APP_NAME = "igd-tool";
 
     public static final int RETURN_PARSE_ERROR = 1;
     public static final int RETURN_UNMATCHED_CLI_ARGS = 2;
+    public static final int RETURN_UPNP_QUERY_ERROR = 3;
+
+    private static boolean appRunning = false;
 
     public static void main(String[] args) {
+        appRunning = true;
+
         CLI cli = new CLI();
         CommandLine cl = null;
 
@@ -26,8 +42,35 @@ public class App {
             System.exit(RETURN_PARSE_ERROR);
         }
 
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                System.out.println("Signaling app to shut down...");
+                appRunning = false;
+            }
+        });
+
         if (cl.hasOption("e")) {
-            System.out.println("TODO: Implement");
+            NatUpnpManager upnpManager = new NatUpnpManager();
+            upnpManager.start();
+            while (appRunning) {
+                try { Thread.sleep(1000); } catch (Exception e) { /* don't care */ }
+                Service wanIPConnectionService = upnpManager.getWANIPConnectionService();
+                if (null != wanIPConnectionService) {
+                    log.info("We have WANIPConnection service");
+                    CompletableFuture<String> future = upnpManager.queryExternalIPAddress();
+                    try {
+                        String ipAddress = future.get();
+                        System.out.println(ipAddress);
+                        System.exit(0);
+                    } catch (Exception e) {
+                        System.err.println("Error sending query: "+ e);
+                        System.exit(RETURN_UPNP_QUERY_ERROR);
+                    }
+                }
+            }
+            log.warn("Shutting down.");
+
         } else if (cl.hasOption("h")) {
             cli.printUsageText(APP_NAME);
         } else {
